ad87174751b91fef3947221522ae49a46acceb06
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2024-10-25 20:44:40 -0300

    auxiliary pages components

---
+++ src/components/pages/PageWithActions.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import StickyActions from '../stickyButtons/StickyActions.vue'
+
+withDefaults(
+  defineProps<{ title?: string }>(),
+  { title: '' }
+)
+</script>
+
+<template>
+  <q-page padding>
+    <h4>{{ title }}</h4>
+
+    <sticky-actions style="z-index: 10">
+      <slot name="actions"></slot>
+    </sticky-actions>
+
+    <slot></slot>
+  </q-page>
+</template>
---
+++ src/components/pages/PageWithAdd.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { RouteLocationRaw } from 'vue-router'
+
+import StickyPath from '../stickyButtons/StickyPath.vue'
+import StickyBtn from '../stickyButtons/StickyBtn.vue'
+
+withDefaults(
+  defineProps<{
+    title?: string
+    to?: RouteLocationRaw | null
+  }>(),
+  {
+    title: '',
+    to: null
+  }
+)
+
+defineEmits<{ click: [void] }>()
+</script>
+
+<template>
+  <q-page padding>
+    <h4>{{ title }}</h4>
+
+    <sticky-path
+      v-if="to"
+      icon="add"
+      :to
+    />
+    <sticky-btn
+      v-else
+      icon="add"
+      @click="$emit('click')"
+    />
+
+    <slot></slot>
+  </q-page>
+  </template>
