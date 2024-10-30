20e312cbed97f50f9066277fa248aa1fb807822b
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2024-10-30 01:04:50 -0300

    envs

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,5 +5,10 @@ declare namespace NodeJS {
     NODE_ENV: string;
     VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
     VUE_ROUTER_BASE: string | undefined;
+    NAME: string;
+    HOST: string;
+    CLIENT_SECRET: string;
+    CLIENT_ID: string;
+    STORAGE_PREFIX: string;
   }
 }
