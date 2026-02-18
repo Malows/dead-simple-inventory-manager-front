3d9c79ca99ad971396b24c86fc3e22b4587bef83
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2026-02-17 21:26:00 -0300

    filterable select

---
+++ src/components/FilterableSelect.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts" generic="T">
+import { ref } from 'vue'
+
+import { SelectOption } from '../types/index'
+
+const props = defineProps<{
+  label: string
+  options: SelectOption<T>[]
+}>()
+
+const model = defineModel<T>()
+
+const filteredOptions = ref<SelectOption<T>[]>(props.options)
+
+function filterFn (val: string, update: (callback: () => void) => void) {
+  update(() => {
+    const needle = val.toLowerCase()
+    filteredOptions.value = !needle
+      ? props.options
+      : props.options.filter(v => v.label.toLowerCase().includes(needle))
+  })
+}
+
+function abortFilterFn () {
+  filteredOptions.value = props.options
+}
+</script>
+
+<template>
+  <q-select
+    v-model="model"
+    :label="label"
+    clearable
+    use-input
+    hide-selected
+    fill-input
+    emit-value
+    map-options
+    input-debounce="0"
+    :options="filteredOptions"
+    @filter="filterFn"
+    @filter-abort="abortFilterFn"
+  >
+    <template v-slot:no-option>
+      <q-item>
+        <q-item-section class="text-grey">
+          No results
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
+</template>
