b6141178642146185aa5f6213658dc54f1cfbf0f
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2026-03-16 16:53:31 -0300

    add interfaces

--- src/types/product.interfaces.ts
+++ src/types/product.interfaces.ts
@@ -1,5 +1,6 @@
 import type { BrandEntity } from './brand.interfaces'
 import type { CategoryEntity } from './category.interfaces'
+import type { StorageLocationEntity } from './storage-location.interfaces'
 import type { SupplierEntity } from './supplier.interfaces'
 
 export interface ProductDTO {
@@ -11,6 +12,7 @@ export interface ProductDTO {
   price: number | null;
   brand_id: number | null;
   supplier_id: number | null;
+  storage_location_id: number | null;
   categories?: number[];
   image_url?: string | null;
 }
@@ -26,12 +28,14 @@ export interface ProductEntity {
   price: number | null;
   brand_id: number | null;
   supplier_id: number | null;
+  storage_location_id: number | null;
   image_url: string | null;
 }
 
 export interface RawProduct extends ProductEntity {
   supplier?: SupplierEntity | null;
   brand?: BrandEntity | null;
+  storage_location?: StorageLocationEntity | null;
   categories?: CategoryEntity[] | null;
   last_price_update: string | null;
   last_stock_update: string | null;
@@ -43,6 +47,7 @@ export interface RawProduct extends ProductEntity {
 export interface Product extends ProductEntity {
   brand: BrandEntity | null;
   supplier: SupplierEntity | null;
+  storage_location: StorageLocationEntity | null;
   categories: CategoryEntity[];
   last_price_update: Date | null;
   last_stock_update: Date | null;
